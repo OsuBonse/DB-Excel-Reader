34380faa7cae5691a5b0b5e5e5f108bdba0d7992
Bonse <84657555+OsuBonse@users.noreply.github.com>
2023-02-16 16:21:39 +0200

    Update DBExcelReader.py

--- DBExcelReader.py
+++ DBExcelReader.py
@@ -5,12 +5,14 @@ workbook = load_workbook(r'...')
 sheet = workbook['...']
 for num in range(40):
         row_num = num + 26 
-        a = sheet['A1' + str(row_num)].value 
+        b = sheet['B1' + str(row_num)].value
+        c = sheet['C1' + str(row_num)].value
+        d = sheet['D1' + str(row_num)].value
 sqlite_connection = sqlite3.connect('ltx.db', timeout=10)
 cursor = sqlite_connection.cursor()
-for sqlite_insert_query in a:
+for sqlite_insert_query in b:
     sqlite_insert_query = """INSERT INTO ltx (Cudnum) VALUES (?)"""
-    cursor.execute(sqlite_insert_query,(a,))
+    cursor.execute(sqlite_insert_query,(b,))
     sqlite_connection.commit()
 cursor.close() 
 sqlite_connection.execute("PRAGMA journal_mode=WAL")
