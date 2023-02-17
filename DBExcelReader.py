244751dda352735d5b5bb856ea5db781df889c0f
Bonse <84657555+OsuBonse@users.noreply.github.com>
2023-02-17 23:13:06 +0300

    Update DBExcelReader.py

--- DBExcelReader.py
+++ DBExcelReader.py
@@ -1,23 +1,31 @@
 from openpyxl import *
 import sqlite3
 
-workbook = load_workbook(r'...')
-sheet = workbook['...']
-for num in range(40):
-        row_num = num + 26 
-        b = sheet['B1' + str(row_num)].value
-        c = sheet['C1' + str(row_num)].value
-        d = sheet['D1' + str(row_num)].value
-sqlite_connection = sqlite3.connect('ltx.db', timeout=10)
+workbook = load_workbook(r'F:\Prog\Py\b1.xlsx')
+sheet = workbook['123']
+sqlite_connection = sqlite3.connect('ltx.db')
 cursor = sqlite_connection.cursor()
-for sqlite_insert_query in b:
-    sqlite_insert_query = """INSERT INTO ltx (Cudnum) VALUES (?)"""
-    cursor.execute(sqlite_insert_query,(b,))
-    sqlite_connection.commit()
+sqlite_insert_query = """INSERT INTO ltx (Id,Cudnum,Koatyy,Area,District,Settlement,Street,TGName,PurposeOfTheAssignment,NameOfTheSite,TransactionType,Price,OwnershipType,RegistrationDate,RegistrationNumber,ValueNGO,EvaluationDate) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)"""
+for i in range(4, 33):
+     a = sheet.cell(row=i, column = 2).value
+     b = sheet.cell(row=i, column = 3).value
+     c = sheet.cell(row=i, column = 4).value
+     d = sheet.cell(row=i, column = 5).value
+     e = sheet.cell(row=i, column = 6).value
+     f = sheet.cell(row=i, column = 7).value
+     g = sheet.cell(row=i, column = 8).value
+     h = sheet.cell(row=i, column = 9).value
+     x = sheet.cell(row=i, column = 10).value
+     j = sheet.cell(row=i, column = 11).value
+     k = sheet.cell(row=i, column = 12).value
+     l = sheet.cell(row=i, column = 13).value
+     m = sheet.cell(row=i, column = 14).value
+     n = sheet.cell(row=i, column = 15).value
+     o = sheet.cell(row=i, column = 16).value
+     p = sheet.cell(row=i, column = 17).value
+     q = sheet.cell(row=i, column = 18).value
+     r = sheet.cell(row=i, column = 19).value
+     cursor.execute(sqlite_insert_query,(a,b,c,d,e,f,g,h,x,j,k,l,m,n,o,p,q))
+     sqlite_connection.commit()
 cursor.close() 
-sqlite_connection.execute("PRAGMA journal_mode=WAL")
 sqlite_connection.close()
-
-
- 
-    
