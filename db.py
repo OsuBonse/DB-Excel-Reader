8141921fd1830e16a78df1aab1663c5bc8f5f677
Bonse <84657555+lxthxrgx@users.noreply.github.com>
2023-03-18 01:18:58 +0300

    Update db.py

--- db.py
+++ db.py
@@ -1,11 +1,27 @@
 import sqlite3
 sqlite_connection = sqlite3.connect('ltx.db')
-sqlite_create_table_query = '''CREATE TABLE ltx (
-                                id INTEGER PRIMARY KEY,
-                               Cudnum TEXT);'''
-
 cursor = sqlite_connection.cursor()
-cursor.execute(sqlite_create_table_query)
+cursor.execute('''CREATE TABLE IF NOT EXISTS mon (
+    Id INTEGER PRIMARY KEY,
+	Cudnum	TEXT,
+	Koatyy	TEXT,
+	Area	TEXT,
+	District	TEXT,
+	Settlement	TEXT,
+	City	TEXT,
+	Street	TEXT,
+	TGName	TEXT,
+	PurposeOfTheAssignment	TEXT,
+    PurposeOfTheAssignmentNormal TEXT,
+	NameOfTheSite	TEXT,
+	TransactionType	TEXT,
+	Price	TEXT,
+	OwnershipType	TEXT,
+	RegistrationDate	TEXT,
+	RegistrationNumber	TEXT,
+	ValueNGO	TEXT,
+	EvaluationDate	TEXT
+)''')
 sqlite_connection.commit()
 
 cursor.close()
